16ce0a6e9adeb0da32f19c8a6bab6ea41716e43f
Michael Checo <checomichael2@gmail.com>
2018-11-25 11:15:09 -0500

    added a post mutation

--- schema.js
+++ schema.js
@@ -1,31 +1,69 @@
-const{ GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLSchema } = require('graphql');
-const axios = require('axios');
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLNonNull
+} = require("graphql");
+const axios = require("axios");
 
 const Person = new GraphQLObjectType({
-    name: 'Person',
-    fields: () => ({
-        birth_year: {type: GraphQLString},
-        name: {type: GraphQLString},
-        mass: {type: GraphQLInt},
-        skin_color: {type: GraphQLString},
-        gender:{ type: GraphQLString},
-        eye_color: {type: GraphQLString},
-    })
-})
+  name: "Person",
+  fields: () => ({
+    birth_year: { type: GraphQLString },
+    name: { type: GraphQLString },
+    mass: { type: GraphQLInt },
+    skin_color: { type: GraphQLString },
+    gender: { type: GraphQLString },
+    eye_color: { type: GraphQLString }
+  })
+});
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        people: {
-            type: new GraphQLList(Person),
+  name: "RootQueryType",
+  fields: {
+    people: {
+      type: new GraphQLList(Person),
+      resolve(parent, args) {
+        return axios
+          .get("https://swapi.co/api/people/")
+          .then(res => res.data.results);
+      }
+    }
+  }
+});
+
+const mutation = new GraphQLObjectType({
+    name:'Mutation',
+    fields:{
+        addPerson: {
+            type: Person,
+            args: {
+                birth_year: { type: new GraphQLNonNull(GraphQLString) },
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                mass: { type: new GraphQLNonNull(GraphQLInt) },
+                skin_color: { type: new GraphQLNonNull(GraphQLString) },
+                gender: { type: new GraphQLNonNull(GraphQLString) },
+                eye_color: { type: new GraphQLNonNull(GraphQLString) }
+            },
             resolve(parent, args) {
-                return axios.get('https://swapi.co/api/people/')
-                .then(res => res.data.results)
+                return axios.post('https://swapi.co/api/people/', {
+                    birth_year: args.birth_year,
+                    name: args.name,
+                    mass: args.mass,
+                    skin_color: args.skin_color,
+                    gender: args.gender,
+                    eye_color: args.eye_color
+                })
+                .then(res => res.data)
             }
         }
     }
 })
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
-})
+  query: RootQuery,
+  mutation
+});
