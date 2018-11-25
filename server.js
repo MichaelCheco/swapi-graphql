60a3afe37b0df52e182e11cc23e1c2a717308828
Michael Checo <checomichael2@gmail.com>
2018-11-24 19:45:47 -0500

    server side dependencies and server.js file configured

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+const PORT = 9000;
+app.listen(PORT, () => console.log(`==^_^== ${PORT} ==^_^==`))
